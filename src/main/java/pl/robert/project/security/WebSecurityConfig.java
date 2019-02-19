b7968f444ad76582bf0809625bc1b7f039f73424
RobertK <robert.krzywina@gmail.com>
2019-02-19 17:57:14 +0100

    error: cannot import style.css

--- src/main/java/pl/robert/project/security/WebSecurityConfig.java
+++ src/main/java/pl/robert/project/security/WebSecurityConfig.java
@@ -1,15 +1,21 @@
 package pl.robert.project.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
+@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/css/**", "/js/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
